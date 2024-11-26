0233b2cc2f1675245696f2f929b1b3dddb79300a
Michal Karwacki <u39_mickar_lbn@technischools.com>
2024-11-26 10:51:39 +0000

    skibidi

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -6,14 +6,24 @@ let greeting = ref('');
 async function handleSubmit(e) {
   e.preventDefault();
   const target = e.target;
-  const name = target.querySelector('#name').value;
-  await my_project_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
+  const chat = target.querySelector('#chat').value;
+  await my_project_backend.save_chat(chat);
 }
 </script>
 
 <template>
+  <main>
+    <img src="/logo2.svg" alt="DFINITY logo" />
+    <br />
+    <br />
+    <form action="#" @submit="handleSubmit">
+      <label for="name">Enter your name: &nbsp;</label>
+      <input id="name" alt="Name" type="text" />
+      <button type="submit">Click Me!</button>
+    </form>
+    <section id="greeting">{{ greeting }}</section>
+  </main>
+</template>
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
